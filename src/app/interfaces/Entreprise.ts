fb37baf97fc74e353b1e84e18f84e83d794d81ec
BD Dinh <bd.dinh@gmail.com>
2022-03-13 14:30:41 -0400

    Ajout des interfaces

---
+++ src/app/interfaces/Entreprise.ts
@@ -0,0 +1,14 @@
+interface Candidat {
+    _id: string;
+    name: string;
+    description: string;
+    imageUrl: string;
+    contactName: string;
+    contactEmail: string;
+    contactPhone: string;
+    address: string;
+    city: string;
+    province: string;
+    postalCode: string;
+    published: boolean;
+}
---
+++ src/app/interfaces/internshipRequest.ts
@@ -0,0 +1,17 @@
+interface InternshipRequest {
+    _id: string;
+    title: string;
+    description: string;
+    enterprise: string;
+    startDate: Date;
+    endDate: Date;
+    program: string;
+    requirements: string;
+    stageType: string;
+    hoursPerWeek: number;
+    additionalInfo: string;
+    skills: string[];
+    published: boolean;
+    paid: string[];
+    active: boolean;
+}

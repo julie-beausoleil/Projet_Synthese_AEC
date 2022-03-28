6ac826b09be1f5e4c8d3304e880efdc4f3a2b5ff
Whistler Clermont <clermontwhistler2@gmail.com>
2022-03-27 20:54:23 -0400

    ajout fiche de stage et editer stage

--- src/app/editer-offre-stage/editer-offre-stage.component.ts
+++ src/app/editer-offre-stage/editer-offre-stage.component.ts
@@ -6,7 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editer-offre-stage.component.sass']
 })
 export class EditerOffreStageComponent implements OnInit {
+  longText = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet quam justo. Aliquam interdum facilisis eros, ut
+  dapibus quam hendrerit nec. Vestibulum vel blandit lorem. Praesent laoreet quis tortor quis pharetra. Nulla dictum erat id
+  nisl pulvinar, ac aliquam lectus laoreet. Aliquam commodo pulvinar odio vitae interdum. Phasellus ut ante efficitur, venenatis
+  purus sit amet, condimentum leo. Pellentesque nulla sem, consectetur.`;
 
+  TexteCourt = `AEC ou équivalent.`;
+
+  TexteCourt2 = `HTML, CSS, JavaScript, React, vue.js, GIT
+                PHP, MySQL. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet quam justo. Aliquam interdum
+                facilisis eros, ut dapibus quam hendrerit nec.`;
+      
+  longText2 = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet quam justo. Aliquam interdum facilisis eros, ut
+               dapibus quam hendrerit nec. Vestibulum vel blandit lorem. Praesent laoreet quis tortor quis pharetra. Nulla dictum erat id
+               nisl pulvinar, ac aliquam lectus laoreet. Aliquam commodo pulvinar odio vitae interdum. Phasellus ut ante efficitur, venenatis
+               purus sit amet, condimentum leo. Pellentesque nulla sem, consectetur`
+
+  
   constructor() { }
 
   ngOnInit(): void {

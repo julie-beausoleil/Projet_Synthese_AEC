e6cd323bc28a7fd1916eb917fc85bc9079efd1fb
Josée Gervais <j_gervais07@hotmail.com>
2022-03-30 18:34:32 -0400

    suite des composants

--- src/app/dernieres-offres/dernieres-offres.component.ts
+++ src/app/dernieres-offres/dernieres-offres.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { OffreStage } from '../dossierDesInterfaces/offre-stage';
+import { OffrestageService } from '../dossierDesServices/offrestage.service';
 
 @Component({
   selector: 'app-dernieres-offres',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dernieres-offres.component.sass']
 })
 export class DernieresOffresComponent implements OnInit {
+  offresStages: OffreStage[] = [];
 
-  constructor() { }
+  constructor(private OffrestageService: OffrestageService) { }
 
   ngOnInit(): void {
+    this.getOffresStages();
+  }
+
+  getOffresStages(): void {
+    this.OffrestageService.getOffreStages()
+      .subscribe((res: OffreStage[]) => (this.offresStages = res));
+  }
+  OnDelete(OffresStages: OffreStage): void {
+    this.OffrestageService.deleteOffreStage(OffresStages._id)
+      .subscribe(_result => this.offresStages = this.offresStages.filter(p => p !== OffresStages));
   }
 
 }

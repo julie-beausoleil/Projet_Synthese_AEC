9bba954ed3485ee24718703063454a771e0ff2d6
stephanieg31 <74106481+stephanieg31@users.noreply.github.com>
2022-04-05 21:01:43 -0400

    correction et ajout de code pour editer une entreprise

--- src/app/editer-entreprise/editer-entreprise.component.ts
+++ src/app/editer-entreprise/editer-entreprise.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Entreprise } from "../dossierDesInterfaces/entreprise"; /* Importation de mon interface - SG */
+import { EntrepriseService } from '../dossierDesServices/entreprise.service'; /* Importantion de mon service -SG  */
+import { NgForm } from '@angular/forms';  // Permet de vérifier si le formulaire est valide 
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-editer-entreprise',
@@ -7,9 +12,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditerEntrepriseComponent implements OnInit {
 
-  constructor() { }
+  entreprise : Entreprise = {
+    _id: " ",
+    name: "",
+    description: "",
+    imageUrl: "",
+    contactName: "",
+    contactEmail: "",
+    contactPhone: "",
+    address: "",
+    city: "",
+    province: "",
+    postalCode: "",
+    published: false,
+  };  
+
+  selectedEntreprise?: Entreprise; 
+  columnsToDisplay = ['actions'];
+
+ 
+  constructor(private entrepriseService : EntrepriseService,
+    private route: ActivatedRoute,
+    private router: Router) { }
+    
 
   ngOnInit(): void {
+    const id = String (this.route.snapshot.params['_id']);
+   this.entrepriseService.getEntreprise(id)
+    .subscribe(resultat => this.entreprise = resultat);
   }
 
+
+  onDelete(entreprise: Entreprise ): void {
+    this.entrepriseService.deleteEntreprise(entreprise._id)
+      .subscribe(() => this.router.navigate(['/']));
+  } 
+
+
+  onSelect(entreprise: Entreprise): void {
+    this.selectedEntreprise = entreprise; 
+  }
+
+  onEdit(entrepriseFormEdition: NgForm): void {
+    if (entrepriseFormEdition.valid && this.selectedEntreprise != null) {
+      this.entrepriseService.editEntreprise(this.selectedEntreprise)
+          .subscribe(() => this.selectedEntreprise = undefined);
+    }
+  }
 }
+
+
+
+
+
+
+
+

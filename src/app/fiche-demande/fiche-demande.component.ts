8d2d95bb1e1074cb443b510103c8d6fb9ec20c50
Josée Gervais <j_gervais07@hotmail.com>
2022-04-13 11:54:46 -0400

    fin de la section demande de stage je peux pas tester mon code l'API fonctionne pas Josée Gervais

--- src/app/fiche-demande/fiche-demande.component.ts
+++ src/app/fiche-demande/fiche-demande.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RequeteStage } from '../dossierDesInterfaces/requete-stage';
 import { RequetestagaService } from '../dossierDesServices/requetestaga.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-fiche-demande',
@@ -10,9 +11,14 @@ import { RequetestagaService } from '../dossierDesServices/requetestaga.service'
 export class FicheDemandeComponent implements OnInit {
  requetesStages: RequeteStage[] = [];
 
-  constructor() { }
+  constructor(private requetestagaService: RequetestagaService, router: Router) { }
 
   ngOnInit(): void {
+    this.getReqStage();
+  }
+  getReqStage(): void {
+    this.requetestagaService.getReqStages()
+      .subscribe((_result: RequeteStage[]) => (this.requetesStages = _result));
   }
 
 }
